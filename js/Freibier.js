417435b13c2553beb65ad5c2be15bf9e319f5b2d
Quentinbur <85225487+Quentinbur@users.noreply.github.com>
2021-06-23 10:51:35 +0200

    burger

--- js/Freibier.js
+++ js/Freibier.js
@@ -1,1 +1,14 @@
 $('.carousel').carousel()
+
+$('a').click(function (){
+if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+&& location.hostname == this.hostname) {
+    var $target = $(this.hash);
+    $target = $target.length && $target || $("[name='" + this.hash.slice(1) +"']");
+    if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body').animate({scrollTop: targetOffset}, 700);
+        return false;
+    }
+}
+})
